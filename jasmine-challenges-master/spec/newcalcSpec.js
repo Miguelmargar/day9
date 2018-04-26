a88762d925b664b377af8dde5a58ca91a51d8310
Miguel Martinez <miguelitalia@hotmail.com>
2018-04-26 13:46:52 +0000

    added test challenge in, created 6 specs with 0 failures built in

---
+++ jasmine-challenges-master/spec/newcalcSpec.js
@@ -0,0 +1,44 @@
+describe("Extra calculator functions", function() {
+    describe("Area function", function() {
+        it("should return 314.16 for a circle with radius 10", function() {
+             expect(areaOfCircle(10)).toBe(314.16);
+        });
+        it("should return 78.54 for a circle with radius 5", function() {
+                expect(areaOfCircle(5)).toBe(78.54);
+         });
+        it("should alert when given two or more arguments", function() {
+            spyOn(window, "alert");
+            areaOfCircle(2, 4);
+            expect(window.alert).toHaveBeenCalledWith("Only one variable needed");
+        });
+        it("Should alert error when given a 0 as an argument",function() {
+            spyOn(window, "alert");
+            areaOfCircle(0);
+            expect(window.alert).toHaveBeenCalledWith("0 value not valid")
+        });
+        it("should alert error when given a negative argument", function() {
+            spyOn(window, "alert");
+            areaOfCircle(-1);
+            expect(window.alert).toHaveBeenCalledWith("Negative value not valid");
+        })
+        it("should alert for NaN", function() {
+            spyOn(window, "alert");
+            areaOfCircle("sting");
+            expect(window.alert).toHaveBeenCalledWith("Argument given in not a number");
+        })
+        
+    });
+    
+    describe("Square Root function", function() {
+         it("should return 3 for square root of 9", function() {
+             expect(squareRoot(9)).toBe(3);
+         });
+    });
+    
+    describe("Square function", function() {
+         it("should return 9 as the square of 3", function() {
+             expect(squareMe(3)).toBe(9);
+         });
+    });
+
+});
---
+++ jasmine-challenges-master/src/calcFunctions.js
@@ -0,0 +1,31 @@
+function areaOfCircle(n) {
+    let result = 3.1416 * (n * n);
+    if (arguments.length > 1) {
+        alert("Only one variable needed");
+    } 
+    if (n == 0) {
+        alert("0 value not valid");
+    } 
+    if (n < 0) {
+        alert("Negative value not valid")
+    } 
+    if (typeof(n) != "number") {
+        alert("Argument given in not a number");
+    } else {
+    return Number(result.toFixed(2));
+    }
+}
+
+
+
+
+
+function squareRoot(n) {
+    let result = Math.sqrt(n);
+    return Number(result.toFixed(2));
+}
+
+function squareMe(n) {
+    let result = 9;
+    return Number(result.toFixed(2));
+}
